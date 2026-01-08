9a5a4c1d08e9f5066708fa24b17cd40e0dd15820
陈子枫(30892983) <chenzifeng004@ke.com>
2026-01-08 10:48:42 +0800

    第一版

---
+++ agentLearn4j/src/main/java/com/czf/agentLearn4j/agentLearn4j/AgentLearn4jApplication.java
@@ -0,0 +1,13 @@
+package com.czf.agentLearn4j.agentLearn4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentLearn4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentLearn4jApplication.class, args);
+	}
+
+}
---
+++ agentLearn4j/src/test/java/com/czf/agentLearn4j/agentLearn4j/AgentLearn4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czf.agentLearn4j.agentLearn4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentLearn4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
